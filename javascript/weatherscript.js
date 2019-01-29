c79abeb0b2c9c8a34660ad43d83d0216a557772f
Kristina Mendoza <kenneth.b.mendoza@gmail.com>
2019-01-29 14:11:34 -0500

    added get location fucntionality

--- javascript/weatherscript.js
+++ javascript/weatherscript.js
@@ -2,10 +2,101 @@ let appId = 'd9b1ca45de6cd478f71d696f620e077b';
 let units = 'metric';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=Paris&units=${units}&APPID=${appId}`;
 window.onload = function PageLoad(){
-    $('#left-container').hide();
     regionCities();
     search();  
     forecast();
+    getLocation();
+}
+function getLocation(){
+    navigator.geolocation.getCurrentPosition(function(position){
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;   
+        
+        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&APPID=${appId}`).then(result => {
+            return result.json();
+        }).then(result => {
+            init(result);
+        }) 
+        function init(resultFromServer){
+            let city = resultFromServer.name;
+            let temp = resultFromServer.main.temp;
+            let low_temp = resultFromServer.main.temp_min;
+            let high_temp = resultFromServer.main.temp_max;
+            let humidity = resultFromServer.main.humidity;
+            let pressure = resultFromServer.main.pressure;
+            let wind_speed = resultFromServer.wind.speed * 1.60934;
+            let wind_deg = resultFromServer.wind.deg;
+            let lat = resultFromServer.coord.lat;
+            let lon = resultFromServer.coord.lon;
+            let desc = resultFromServer.weather[0].description;  
+            
+            $(function(){
+                    $.get(`http://api.timezonedb.com/v2.1/get-time-zone?key=4DVH3B4RX5SQ&format=json&by=position&lat=${lat}&position&lng=${lon}`)
+                    .done(function(json){
+                        let time = new Date(json.timestamp);
+                        let country = json.countryName;
+                        document.getElementById('left-time').innerHTML = timeFormatUTC(time);   
+                        document.getElementById('left-city').innerHTML = city + ", " + country;
+                    });
+            }); 
+            console.log(resultFromServer.weather[0].main);
+            switch (resultFromServer.weather[0].main){
+                case 'Clear':
+                        document.getElementById('left-icon').innerHTML = '<i class="fal fa-sun-cloud"></i>';
+                        break;
+                case 'Clouds':
+                        document.getElementById('left-icon').innerHTML = '<i class="fal fa-clouds"></i>';
+                        break;
+                case 'Thunderstorm':
+                        document.getElementById('left-icon').innerHTML = '<i class="fal fa-thunderstorm"></i>';
+                        break;
+                case 'Snow':
+                        document.getElementById('left-icon').innerHTML = '<i class="fal fa-snowflakes"></i>';
+                        break;
+                case 'Rain':
+                case 'Drizzle':
+                case 'Mist':
+                        document.getElementById('left-icon').innerHTML = '<i class="fal fa-cloud-rain"></i>';
+                        break;
+                default:
+                        break;  
+            }
+            document.getElementById('left-temp').innerHTML = temp.toFixed() + "&#8451";
+            document.getElementById('left-date').innerHTML = dateFormat(resultFromServer.dt);
+            document.getElementById('high-temp').innerHTML = 'High: ' + high_temp.toFixed(0) + "&#8451";
+            document.getElementById('low-temp').innerHTML = 'Low: ' + low_temp.toFixed(0) + "&#8451";
+            document.getElementById('humidity').innerHTML = 'Humidity: ' + humidity + "%";
+            document.getElementById('pressure').innerHTML = 'Pressure: ' + pressure + " hPa";
+            document.getElementById('wind-speed').innerHTML = 'Wind: ' + wind_speed.toFixed(0) + "km/h";
+            document.getElementById('wind-deg').innerHTML = 'Degree: ' + wind_deg.toFixed(0) + "&#xB0";                
+            document.getElementById('sunrise').innerHTML = 'Sunrise: ' + timeFormat(resultFromServer.sys.sunrise);                
+            document.getElementById('sunset').innerHTML = 'Sunset: ' + timeFormat(resultFromServer.sys.sunset);
+            document.getElementById('time1').innerHTML = 'Now';   
+            document.getElementById('desc1').innerHTML = desc.charAt(0).toUpperCase() + desc.slice(1);   
+            document.getElementById('temp1').innerHTML = temp.toFixed(0) + "&#8451";
+
+            fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=7&units=${units}&APPID=${appId}`).then(result => {
+                return result.json();
+            }).then(result => {
+                init(result);
+            })
+            function init(resultFromServer){
+                for(let i = 0; i < resultFromServer.list.length; i++){
+                    let time = resultFromServer.list[i].dt;
+                    let desc = resultFromServer.list[i].weather[0].description;  
+                    let temp = resultFromServer.list[i].main.temp;  
+                    let timeID = 'time'+[i+2];
+                    let descID = 'desc'+[i+2];
+                    let tempID = 'temp'+[i+2];
+
+                    document.getElementById(timeID).innerHTML = timeFormat(time);   
+                    document.getElementById(descID).innerHTML = desc.charAt(0).toUpperCase() + desc.slice(1);   
+                    document.getElementById(tempID).innerHTML = temp.toFixed(0) + "&#8451";
+                }
+            }
+        }
+    }); 
+    $("#left-section").fadeIn(1000);
 }
 function forecast(){
     $(document).ready(function(){
@@ -26,8 +117,7 @@ function forecast(){
     }); 
     $("#search").on("keydown",function search(e) {
         if(e.keyCode == 13) {
-            $('#left-container').hide();
-            $('#left-container').fadeIn(1500);
+            $('#right-container').fadeIn(1000);
             let searchIn = $(this).val();
             
             fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${searchIn}&cnt=7&units=${units}&APPID=${appId}`).then(result => {
@@ -55,8 +145,7 @@ function forecast(){
 function search() {
      $("#search").on("keydown",function search(e) {
         if(e.keyCode == 13) {
-            $('#left-container').hide();
-            $('#left-container').show();
+            $('#left-container').fadeIn(1000);
             let searchIn = $(this).val();
             
             fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchIn}&units=${units}&APPID=${appId}`).then(result => {
@@ -111,14 +200,14 @@ function search() {
                 }
                 document.getElementById('left-temp').innerHTML = temp.toFixed() + "&#8451";
                 document.getElementById('left-date').innerHTML = dateFormat(resultFromServer.dt);
-                document.getElementById('high-temp').innerHTML = high_temp.toFixed(0) + "&#8451";
-                document.getElementById('low-temp').innerHTML = low_temp.toFixed(0) + "&#8451";
-                document.getElementById('humidity').innerHTML = humidity + "%";
-                document.getElementById('pressure').innerHTML = pressure + " hPa";
-                document.getElementById('wind-speed').innerHTML = wind_speed.toFixed(0) + "km/h";
-                document.getElementById('wind-deg').innerHTML = wind_deg.toFixed(0) + "&#xB0";                
-                document.getElementById('sunrise').innerHTML = timeFormat(resultFromServer.sys.sunrise);                
-                document.getElementById('sunset').innerHTML = timeFormat(resultFromServer.sys.sunset);  
+                document.getElementById('high-temp').innerHTML = 'High: ' + high_temp.toFixed(0) + "&#8451";
+                document.getElementById('low-temp').innerHTML = 'Low: ' + low_temp.toFixed(0) + "&#8451";
+                document.getElementById('humidity').innerHTML = 'Humidity: ' + humidity + "%";
+                document.getElementById('pressure').innerHTML = 'Pressure: ' + pressure + " hPa";
+                document.getElementById('wind-speed').innerHTML = 'Wind: ' + wind_speed.toFixed(0) + "km/h";
+                document.getElementById('wind-deg').innerHTML = 'Degree: ' + wind_deg.toFixed(0) + "&#xB0";                
+                document.getElementById('sunrise').innerHTML = 'Sunrise: ' + timeFormat(resultFromServer.sys.sunrise);                
+                document.getElementById('sunset').innerHTML = 'Sunset: ' + timeFormat(resultFromServer.sys.sunset);  
                 document.getElementById('time1').innerHTML = 'Now';   
                 document.getElementById('desc1').innerHTML = desc.charAt(0).toUpperCase() + desc.slice(1);   
                 document.getElementById('temp1').innerHTML = temp.toFixed(0) + "&#8451";
